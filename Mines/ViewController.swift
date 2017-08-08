35fcb5e78d79c6d55e5247079e1888387498c95f
Haven Barnes <haven38@gmail.com>
2017-08-08 13:59:33 -0500

    Initial Commit

---
+++ Mines/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mines
+//
+//  Created by Haven Barnes on 8/8/17.
+//  Copyright © 2017 Azing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
